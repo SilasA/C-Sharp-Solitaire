42ce4d89d85d56cb386ab71037554a5e73eacf20
sagnew <sagnew@PRG23657>
2016-09-15 14:09:28 -0400

    Added Card and Deck classes

---
+++ CS Solitare/Card.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace CS_Solitare
+{
+    class Card
+    {
+        const int CARDSIZE_X = 140;
+        const int CARDSIZE_Y = 190;
+
+        public enum Suit
+        {
+            Clubs,
+            Hearts,
+            Diamonds,
+            Spades
+        }
+
+        Suit cardSuit;
+        public Suit CardSuit
+        {
+            get;
+            set;
+        }
+        int cardId;
+
+        int parentDeckId;
+
+        Rectangle currentRect;
+        Rectangle originalRect;
+        Rectangle frameRect;
+
+        Card upperCard;
+        public Card UpperCard
+        {
+            get { return upperCard; }
+            set { upperCard = value; }
+        }
+        Card lowerCard;
+        public Card LowerCard
+        {
+            get { return lowerCard; }
+            set { lowerCard = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Card()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="frameRect"></param>
+        /// <param name="suit"></param>
+        /// <param name="cardId"></param>
+        /// <param name="parentDeckId"></param>
+        public Card(Rectangle rect, Rectangle frameRect, Suit suit, int cardId, int parentDeckId)
+        {
+            currentRect = rect;
+            originalRect = rect;
+            this.frameRect = frameRect;
+            cardSuit = suit;
+            this.cardId = cardId;
+            this.parentDeckId = parentDeckId;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Rectangle DrawFrom()
+        {
+            return frameRect;
+        }
+    }
+}
---
+++ CS Solitare/Deck.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CS_Solitare
+{
+    class Deck
+    {
+        protected List<Card> cardList;
+
+        public enum DeckType
+        {
+            Hand,
+            Waste,
+            Tableau,
+            Foundation
+        }
+        protected DeckType type;
+
+        protected int id;
+
+        protected Rectangle location;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Deck()
+        {
+            cardList = new List<Card>();
+        }
+
+        /// <summary>
+        /// s
+        /// </summary>
+        /// <param name="id"></param>
+        public Deck(int id)
+        {
+            this.id = id;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void AppendCard(Card card)
+        {
+            cardList.Add(card);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public virtual void Draw(SpriteBatch spriteBatch, Texture2D texture)
+        {
+
+        }
+    }
+}
